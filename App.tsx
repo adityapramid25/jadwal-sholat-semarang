379898b809698058ff4b0c0249e60cbef5c12af9
adityapramid25 <adityapramid25@gmail.com>
2025-09-26 03:34:34 +0700

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -167,7 +167,7 @@ const App: React.FC = () => {
                         </div>
                     </div>
                     <div className="flex items-center justify-center gap-2 mt-4 opacity-60">
-                         <LogoIcon />
+                         <img src="logosmk.png" style="width: 20px; height: auto;"></img>
                          <p className="text-xs">SMKN 9 SEMARANG</p>
                     </div>
                 </footer>
